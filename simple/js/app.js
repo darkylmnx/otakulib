3d3aa8c7cb05dee85ebd3ed7e34e07ba2d5177bd
darkylmnx <darkun91@gmail.com>
2017-07-17 01:55:23 +0200

    [CHG] form > added new entry logic

--- simple/js/app.js
+++ simple/js/app.js
@@ -2,13 +2,38 @@ var app = new Vue({
     el: '#main',
 
     data: {
-        series: []
+        series: [],
+        new_name: '',
+        new_type: 'anime',
+        new_total: '',
+        new_seen: ''
     },
 
     created() {
         this.series = getLocalJson('series');
+    },
+
+    methods: {
+        addSerie: function() {
+            var serie = {
+                name: this.new_name,
+                type: this.new_type,
+                total: this.new_total,
+                seen: this.new_seen
+            };
+
+            this.series.push(serie);
+            this.resetForm();
+        },
+
+        resetForm: function() {
+            this.new_name = '';
+            this.new_type = 'anime';
+            this.new_total = '';
+            this.new_seen = '';
+        }
     }
-})
+});
 
 function getLocalJson(key) {
     var data = localStorage.getItem(key);
