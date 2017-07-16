d9811a57b52ac2fee7e805669c41921d351bbbf9
darkylmnx <darkun91@gmail.com>
2017-07-17 01:47:59 +0200

    [CHG] list > setup vue instance + added empty message condition

--- simple/js/app.js
+++ simple/js/app.js
@@ -1,1 +1,23 @@
+var app = new Vue({
+    el: '#main',
 
+    data: {
+        series: []
+    },
+
+    created() {
+        this.series = getLocalJson('series');
+    }
+})
+
+function getLocalJson(key) {
+    var data = localStorage.getItem(key);
+
+    if (data) {
+        data = JSON.parse(data);
+    } else {
+        data = [];
+    }
+
+    return data;
+}
